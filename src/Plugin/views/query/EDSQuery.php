167b1fe3d2d63694cdd9b7aee91c4e433c9e1f66
Wahaj Hussain <wahajhssn@gmail.com>
2026-05-05 13:45:14 -0500

    Fixed compatibility issues with drupal 11

--- src/Plugin/views/query/EDSQuery.php
+++ src/Plugin/views/query/EDSQuery.php
@@ -89,7 +89,7 @@ class EDSQuery extends QueryPluginBase
      */
     public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self
     {
-        return new static(
+        return new self(
             $configuration,
             $plugin_id,
             $plugin_definition,
